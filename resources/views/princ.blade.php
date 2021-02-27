9bb154c88e7edef5ee5201bc33e53cb1abe85c68
JavierPineyro <javidunam22@gmail.com>
2021-02-27 16:23:06 -0300

    Primer commit de Javi

---
+++ resources/views/princ.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pruebas Css y Js</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <script type="text/javascript" src="{!! asset('js/main.js') !!}" async></script>
+    <?php
+        // {{HTML::style('css/style.css');}}
+        // HTML::script('js/main.js');
+    ?>
+</head>
+<body>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('princ');
 });
+
+// hay que poner dos comandos despues de crear
+// 1) composer install
+// 2) cp .env.example .env
+// 3) php artisan key:generate
